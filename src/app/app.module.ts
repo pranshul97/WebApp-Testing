f9422effe132252bcfdbeb146d7d140165f7b187
Bhavya <BHAVYA.Kumar@lntinfotech.com>
2020-08-20 00:48:34 +0530

    commit by bhavya

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MidPartComponent } from './mid-part/mid-part.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MidPartComponent
   ],
   imports: [
     BrowserModule,
